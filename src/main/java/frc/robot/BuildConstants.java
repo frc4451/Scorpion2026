39a5bd8553d835580fed32afd7309cd557540b46
becantbuzz <beyoncabanton08@gmail.com>
2026-02-04 16:35:59 -0500

    Added Superstructures

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Scorpion2026";
+  public static final String MAVEN_NAME = "Scorpion2026-5";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "88e6705cdb61002cfa0ba9e420695ead3151585f";
-  public static final String GIT_DATE = "2026-01-24 13:04:35 EST";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "e369a09c5d8cc2680b26831f4292b3ae0f66cc99";
+  public static final String GIT_DATE = "2026-01-26 19:09:29 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-26 19:00:46 EST";
-  public static final long BUILD_UNIX_TIME = 1769472046244L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2026-02-04 16:26:44 EST";
+  public static final long BUILD_UNIX_TIME = 1770240404379L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
