907804f22fb3e3ae1ebfcbdba08666b15e1a6358
Judson James <judsonjames96@gmail.com>
2026-03-11 18:38:16 -0400

    Restored default PathPlanner Config

--- src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
@@ -2,7 +2,9 @@ package frc.robot.subsystems.drive;
 
 import static frc.robot.subsystems.drive.DriveConstants.FF_RAMP_RATE;
 
+import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPLTVController;
 import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -63,43 +65,42 @@ public class DriveSubsystem extends SubsystemBase {
       // Handle exception as needed
       e.printStackTrace();
     }
+    // }
+
+    // public void driveRobotRelative(ChassisSpeeds speeds) {
+    //   var moduleStates = kinematics.toSwerveModuleStates(speeds);
+    //   SwerveDriveKinematics.desaturateWheelSpeeds(moduleStates, kMaxSpeed);
+
+    //   frontLeft.
+    // }
+
+    //   // Configure AutoBuilder last
+    AutoBuilder.configure(
+        this::getPose, // Robot pose supplier
+        this::resetPose, // Method to reset odometry (will be called if your auto has a starting
+        this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+        (speeds, feedforwards) ->
+            runClosedLoop(
+                speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds.
+        // Also optionally outputs individual module feedforwards
+        new PPLTVController(
+            0.02), // PPLTVController is the built in path following controller for differential
+        // drive trains
+        DriveConstants.ppConfig, // The robot configuration
+        () -> {
+          // Boolean supplier that controls when the path will be mirrored for the red alliance
+          // This will flip the path being followed to the red side of the field.
+          // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+          var alliance = DriverStation.getAlliance();
+          if (alliance.isPresent()) {
+            return alliance.get() == DriverStation.Alliance.Red;
+          }
+          return false;
+        },
+        this // Reference to this subsystem to set requirements
+        );
   }
-
-  // public void driveRobotRelative(ChassisSpeeds speeds) {
-  //   var moduleStates = kinematics.toSwerveModuleStates(speeds);
-  //   SwerveDriveKinematics.desaturateWheelSpeeds(moduleStates, kMaxSpeed);
-
-  //   frontLeft.
-  // }
-
-  //   // Configure AutoBuilder last
-  //   AutoBuilder.configure(
-  //       this::getPose, // Robot pose supplier
-  //       this::setPose, // Method to reset odometry (will be called if your auto has a starting
-  // pose)
-  //       this::getRobotRelativeSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-  //       (speeds, feedforwards) ->
-  //           runClosedLoop(
-  //               speeds), // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds.
-  //       // Also optionally outputs individual module feedforwards
-  //       new PPLTVController(
-  //           0.02), // PPLTVController is the built in path following controller for differential
-  //       // drive trains
-  //       ppConfig, // The robot configuration
-  //       () -> {
-  //         // Boolean supplier that controls when the path will be mirrored for the red alliance
-  //         // This will flip the path being followed to the red side of the field.
-  //         // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
-
-  //         var alliance = DriverStation.getAlliance();
-  //         if (alliance.isPresent()) {
-  //           return alliance.get() == DriverStation.Alliance.Red;
-  //         }
-  //         return false;
-  //       },
-  //       this // Reference to this subsystem to set requirements
-  //       );
-  // }
 
   @Override
   public void periodic() {
