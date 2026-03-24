8b2b47489537ff63ece0f1361dabc56332c35a8b
4451 driver station <4451driverstation@gmail.com>
2026-03-24 17:56:20 -0400

    The beginning of Launcher Interpolation sorry I have to leave early.

--- src/main/java/frc/robot/subsystems/superstructure/SuperstructureConstants.java
+++ src/main/java/frc/robot/subsystems/superstructure/SuperstructureConstants.java
@@ -16,7 +16,7 @@ public class SuperstructureConstants {
   public static final double intakeLauncherMotorReduction = 1.0;
   public static final int intakeLauncherCurrentLimit = 60;
 
-  public static final double kLauncherKp = 0.00025;
+  public static final double kLauncherKp = 0.001;
   public static final double kLauncherKi = 0.0;
   public static final double kLauncherKd = 0.0;
   public static final double kLauncherKV = 0.0001735;
@@ -26,8 +26,7 @@ public class SuperstructureConstants {
   public static final double launchingFeederVoltage = -7.0;
   public static final double launchingLauncherVoltage = -7.0;
   public static final double spinUpFeederVoltage = 1.0;
-  public static final double spinUpSeconds = 1.8;
+  public static final double spinUpSeconds = 2.5;
 }
-// 56.5 in from hub
 // 26.5 in length
 // 26.1-ish in width
